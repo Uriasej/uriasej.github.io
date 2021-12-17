697b9ecc6d1857a114ad3a8a169d7d9130e4f0a1
Uriasej <82612228+Uriasej@users.noreply.github.com>
2021-12-17 12:03:31 -0600

    Add files via upload

---
+++ infMario/scripts/core.js
@@ -0,0 +1,7 @@
+/**
+	Define the Enjine namespace and any other miscellaneous setup.
+	Code by Rob Kleffner, 2011
+    Modified by Eden Urias, 2021
+*/
+
+var Enjine = {};
---
+++ infMario/scripts/drawable.js
@@ -0,0 +1,12 @@
+/**
+	Code by Rob Kleffner, 2011
+    Modified by Eden Urias, 2021
+*/
+
+Enjine.Drawable = function() {
+    this.ZOrder = 0;
+};
+
+Enjine.Drawable.prototype = {
+    Draw: function(context) { }
+};
